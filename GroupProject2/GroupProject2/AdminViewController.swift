8e03363c2b236053c9eb13a2ec0ca5feb374d3e5
FreeMasen <RFM@localhost>
2016-03-18 22:52:44 -0500

    re-arranged storyboard

--- GroupProject2/GroupProject2/AdminViewController.swift
+++ GroupProject2/GroupProject2/AdminViewController.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class AdminViewController: UIViewController {
     
-    
-   
     @IBOutlet weak var editMenuItemsButton: UIBarButtonItem!
     
     @IBOutlet weak var assignServersButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -26,16 +23,4 @@ class AdminViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
